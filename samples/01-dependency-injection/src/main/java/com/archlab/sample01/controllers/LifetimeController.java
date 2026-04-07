e3ece7f613a909e79f4a26462f8a7cb96530c8c5
paulocastelo <oluap.castelo@gmail.com>
2026-04-07 10:35:45 -0300

    fix: refresh prototype instances per request

--- samples/01-dependency-injection/src/main/java/com/archlab/sample01/controllers/LifetimeController.java
+++ samples/01-dependency-injection/src/main/java/com/archlab/sample01/controllers/LifetimeController.java
@@ -4,6 +4,7 @@ import com.archlab.sample01.services.PrototypeGreetingService;
 import com.archlab.sample01.services.RequestGreetingService;
 import com.archlab.sample01.services.SingletonGreetingService;
 import java.util.Map;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,22 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/lifetime")
 public class LifetimeController {
-    private final PrototypeGreetingService prototype1;
-    private final PrototypeGreetingService prototype2;
+    private final ObjectProvider<PrototypeGreetingService> prototypeProvider;
     private final SingletonGreetingService singleton1;
     private final SingletonGreetingService singleton2;
     private final RequestGreetingService request1;
     private final RequestGreetingService request2;
 
     public LifetimeController(
-            PrototypeGreetingService prototype1,
-            PrototypeGreetingService prototype2,
+            ObjectProvider<PrototypeGreetingService> prototypeProvider,
             SingletonGreetingService singleton1,
             SingletonGreetingService singleton2,
             RequestGreetingService request1,
             RequestGreetingService request2) {
-        this.prototype1 = prototype1;
-        this.prototype2 = prototype2;
+        this.prototypeProvider = prototypeProvider;
         this.singleton1 = singleton1;
         this.singleton2 = singleton2;
         this.request1 = request1;
@@ -35,6 +33,9 @@ public class LifetimeController {
 
     @GetMapping
     public Map<String, String> show() {
+        var prototype1 = prototypeProvider.getObject();
+        var prototype2 = prototypeProvider.getObject();
+
         return Map.of(
                 "prototype1", prototype1.greet(),
                 "prototype2", prototype2.greet(),
